6682aaf0c10e1bec06793a6872b85ea03d0ddb9b
chrislucas <christoffer.luccas@gmail.com>
2018-11-13 18:20:30 -0200

    estudos

--- from-docs/Basics/src/basics/function/bits/Bitwise.kt
+++ from-docs/Basics/src/basics/function/bits/Bitwise.kt
@@ -27,23 +27,34 @@ infix fun Int.set(ith: Int) : Int = (1 shl ith) or this
 
 infix fun Int.toggle(ith: Int) : Int = (1 shl ith) xor this
 
+infix fun Int.checkkBit(ith: Int) : Int = (this shr ith) and 1
 
-fun main(args: Array<String>) {
+fun Int.isPowerOf2() : Boolean = (this and this-1) == 0
 
+
+fun testLambdaExpression() {
     println(setBit(10, 2))
     println(unsetBit(15, 3))
     println(unsetBit(255, 7))
-
-    println(255 unset 7)
-    println(200 set 7)
-
     println(toggleBit(128, 0))
     println(toggleBit(128, 1))
     println(checkBit(128, 6))
     println(isPowerOf2(120))
     println(isPowerOf2(128))
-
     println("${10 shr 3}, ${checkBit(10, 3)}")
+}
+
+
+fun testInfixLambdaExpression() {
+    println(255 unset 7)
+    println(200 set 7)
+    println(128 toggle 7)
+    println(128 checkkBit 6)
+    println(128.isPowerOf2())
+}
+
+fun main(args: Array<String>) {
+    testInfixLambdaExpression()
 
 }
 
