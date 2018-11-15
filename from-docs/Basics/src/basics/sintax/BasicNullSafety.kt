c631e1c84809a35c09ee48c5bae95411c8561e5c
chrislucas <christoffer.luccas@gmail.com>
2018-11-15 17:53:31 -0200

    estudo sobre kotlin

--- from-docs/Basics/src/basics/sintax/BasicNullSafety.kt
+++ from-docs/Basics/src/basics/sintax/BasicNullSafety.kt
@@ -4,6 +4,47 @@ package basics.sintax
  * https://kotlinlang.org/docs/reference/null-safety.html
  * */
 
-fun main(args: Array<String>) {
 
+fun safeConcat(a: String?, b: String) : String? {
+    return a?.plus(b)
+}
+
+fun testNullableVar() {
+    // uso de !! pois o metodo retorna null
+    val data = readLine()!!
+    println(data)
+
+    // Operador ? indica que uma variavel pode aceitar null (nullable(
+
+    var a: String? = null
+    a = "teste"
+
+    // a linha abaixo nao eh compilada pois para definir
+    // uma variavel como null eh necessario definir seu tipo usando "?"
+    //var b: String = null
+
+    var b : String = "teste"
+    // a linha abaixo nao eh compilada pois a variavel nao foi definida tal que sceitasse um valor null
+    // b = null
+
+    a = null
+    println(a)
+    // Safe calls
+    println(safeConcat("teste: ", data)!!)
+    println(safeConcat(a, data)!!)
+}
+
+
+/**
+ *  Uma forma de executar uma operacao somente em valores nao nulos eh usando o operador
+ *  let
+ * */
+fun testLetOperator() {
+
+    listOf<String?>("Chris", "Lucas", "Fermamdes", "Santos", null)
+            .forEach { it?.let { println("$it size: ${it.length}") } }
+}
+
+fun main(args: Array<String>) {
+    testLetOperator()
 }
