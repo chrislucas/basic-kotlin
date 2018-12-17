8b706cfd6abf9016791be1ec8d664c4c89447271
chrislucas <christoffer.luccas@gmail.com>
2018-12-17 17:31:28 -0200

    estudos

---
+++ from-docs/Basics/src/basics/math/geom/XrangePancakes.kt
@@ -0,0 +1,31 @@
+package basics.ma th.geom
+/**
+ * https://www.hackerrank.com/challenges/xrange-and-pizza/problem
+ * */
+
+fun angle(n: Int) = 360 / (2*n)
+
+fun rotator(k: Int) = (360*k) / 2
+
+
+fun execute(n: Int, m: Int) {
+    val size = if (n and 1 == 1) n else (2 * n)
+
+    var rotates =  Array<Int>(size) { 0 }
+    var flips =  Array<Int>(size) { 0 }
+
+    for ( i in 0 until m) {
+        val values = readLine()?.split(" ")?.map { it.toInt() }?.toTypedArray()
+        val type = values?.get(0) ?: 0
+        val k = values?.get(1) ?: 0
+
+        if (i == 1)
+            rotates[k] += 1
+        else
+            flips[k] += 1
+    }
+}
+
+fun main(args: Array<String>) {
+
+}
