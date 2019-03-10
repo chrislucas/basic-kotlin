92fa0ff85738d683ac0718fa5dc54eca1f48a14f
chrislucas <christoffer.luccas@gmail.com>
2019-03-10 15:33:44 -0300

    merging

---
+++ TutorialScopingFunctions/src/functions/ScopingFunctionLet.kt
@@ -0,0 +1,12 @@
+package functions
+
+
+/**
+ * Codigo baseado no tutorial https://medium.com/luizalabs/kotlin-let-run-with-also-ou-apply-24e8745f12fd
+ *
+ * Vamos ver o que mais da pra fazer a partir da ideia inicial da autora
+ * */
+
+fun main(args: Array<String>) {
+
+}
