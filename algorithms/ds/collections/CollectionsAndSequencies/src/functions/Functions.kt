a62fa728dc4b048c7e2a00a93829b1cca65098ee
Christoffer Lucas <christoffer.luccas@gmail.com>
2019-08-13 08:02:10 -0300

    estudo sobre funcoes, lambda expressions, anonymous func

--- algorithms/ds/collections/CollectionsAndSequencies/src/functions/Functions.kt
+++ algorithms/ds/collections/CollectionsAndSequencies/src/functions/Functions.kt
@@ -2,16 +2,49 @@ package functions
 
 // https://kotlinlang.org/docs/reference/functions.html
 
-// lambda
+// lambda expression sintaxex
 val modI = {p: Int, q: Int -> p % q}
+
+// outra forma de escrever lambdas
+val modI2: (Int, Int) -> Int = {p, q -> p %q}
 
 // anonymous fun com single expression
 val anounModI = fun(p: Int, q: Int) =  p % q
 
-val modAlias : Mod = {p: Int, q: Int -> p % q}
+// usando typealias para definir tipos
+val modAlias : BinOpInt = {p: Int, q: Int -> p % q}
+
+
+object Test {
+
+    @JvmStatic
+    fun defaultParamsValues(a: Int, b: Int = 1, c: Int) {
+        println(a+b+c)
+    }
+
+    fun sameFn() {
+        println(modI(10, 7))
+        println(anounModI(10, 7))
+        println(modAlias(10, 7))
+    }
+
+    fun lambdaFn(fn: () -> Unit) {
+        fn()
+    }
+
+    fun lambdaFn2(fn : (Int, Int) -> Unit, a: Int, b: Int) {
+        fn(a,b)
+    }
+
+}
+
 
 fun main(args: Array<String>) {
-    println(modI(10, 7))
-    println(anounModI(10, 7))
-    println(modAlias(10, 7))
+    //Test.defaultParamsValues(0, c = 10)
+    Test.lambdaFn { println(0xff and 0xdd) }
+
+    Test.lambdaFn2 ({p: Int, q: Int -> p * q }, 6, 7)
+
+    val fn: (Int, Int) -> Unit = { p, q -> println( p * q) }
+    Test.lambdaFn2 (fn, 6, 7)
 }
--- algorithms/ds/collections/CollectionsAndSequencies/src/functions/Typealias.kt
+++ algorithms/ds/collections/CollectionsAndSequencies/src/functions/Typealias.kt
@@ -2,6 +2,6 @@ package functions
 
 // https://kotlinlang.org/docs/reference/lambdas.html
 
-typealias Mod = (Int, Int) -> Int
+typealias BinOpInt = (Int, Int) -> Int
 
 typealias BoolBinaryOp<T> = (T, T) -> Boolean
